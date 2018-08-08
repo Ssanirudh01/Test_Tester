8e22d01c0b77602a7ef439f6f875152044234cb6
abhijithswamy7 <abhijith.swamy7@gmail.com>
2018-08-08 08:18:17 +0530

    Update Testing.java

--- Testing/src/test/java/testing/Testing.java
+++ Testing/src/test/java/testing/Testing.java
@@ -22,10 +22,10 @@ public class Testing {
 	  }
 	  @BeforeClass
 	  public void beforeClass() {
-		  System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
-		  //System.setProperty("webdriver.gecko.driver", "E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");
+		  //System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+		  System.setProperty("webdriver.gecko.driver", "E:\\backup\\geckodriver-v0.21.0-win64\\geckodriver.exe");
 		  driver= new FirefoxDriver();
-		  driver.get("http://ec2-18-219-182-133.us-east-2.compute.amazonaws.com:8090/TestWebapp");
+		  driver.get("http://localhost:8090/manager/TestWebapp");
 		  
 	  }
 
